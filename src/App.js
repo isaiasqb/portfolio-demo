5c5ba29bfaa5f938366dd3ce9e1a2884616b6511
IsaiasQB <isaias.qb@hotmail.com>
2022-07-17 03:43:22 -0400

    defined the props in the parent App.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 //This App.js file is the center of the application. The root component, or the wrapper component 
 // that houses all of the other components. To effect any change on the application, we need to either modify this file or add components inside it.
 import './App.css';
-import React from 'react'
+import React, { useState } from 'react';
 
 //importing components
 import About from './components/About';
@@ -9,9 +9,25 @@ import Nav from './components/Nav';
 import Portfolio from './components/Portfolio';
 
 function App() {
+
+  const [categories] = useState([
+    {
+      name: "productivity",
+      description: "A few examples of recent apps that I've built, fully functional and guaranteed to boost your productivity.",
+    },
+    { name: "miscellaneous", 
+    description: "A few examples of recent apps that have enjoyment and convenience as the main focus, fully functional and built form scratch" }
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
-      <Nav></Nav>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
       <main>
         <Portfolio></Portfolio>
         <About></About>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,23 +1,14 @@
 //  The Nav component will return a single JSX element, the <header>.
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import {capitalizeFirstLetter } from '../../utils/helpers'
 
-function categorySelected (name) {
-  console.log(`${name} clicked`)
-}
+function Nav(props) {
 
-function Nav() {
-
-  const [categories] = useState([
-    {
-      name: "productivity",
-      description: "A few examples of recent apps that I've built, fully functional and guaranteed to boost your productivity.",
-    },
-    { name: "miscellaneous", 
-    description: "A few examples of recent apps that have enjoyment and convenience as the main focus, fully functional and built form scratch" }
-  ]);
-
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const {
+    categories = [],
+    setCurrentCategory,
+    currentCategory,
+  } = props;
 
   return (
 <header>
