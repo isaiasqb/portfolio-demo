93ab23d360b046737f7a3c3bf352d0da9e3308d1
IsaiasQB <isaias.qb@hotmail.com>
2022-07-18 01:57:26 -0400

    Updates

---
+++ precache-manifest.5f7565f42a163b5d7ea79d08621c7ec5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "82aab24744c39283691b",
+    "url": "/portfolio-demo/static/css/main.4c196f43.chunk.css"
+  },
+  {
+    "revision": "82aab24744c39283691b",
+    "url": "/portfolio-demo/static/js/main.82aab247.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/portfolio-demo/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "803af5c62d3cc7c0b817",
+    "url": "/portfolio-demo/static/js/2.803af5c6.chunk.js"
+  },
+  {
+    "revision": "1bfab3a65d57666f1aa0",
+    "url": "/portfolio-demo/static/js/runtime~main.1bfab3a6.js"
+  },
+  {
+    "revision": "e4c6ca5cfce3dcf19ee471fb8b5d5629",
+    "url": "/portfolio-demo/static/media/avatar.e4c6ca5c.jpg"
+  },
+  {
+    "revision": "efad3568d3440ab8ec3c1d753f714e48",
+    "url": "/portfolio-demo/static/media/0.efad3568.png"
+  },
+  {
+    "revision": "430cbb41d68f96779aaa93f56e2c396a",
+    "url": "/portfolio-demo/static/media/1.430cbb41.png"
+  },
+  {
+    "revision": "8800091f1b50f542eda02b27c4252752",
+    "url": "/portfolio-demo/static/media/2.8800091f.png"
+  },
+  {
+    "revision": "9b305f932546297e98f505f72c5b5283",
+    "url": "/portfolio-demo/static/media/0.9b305f93.png"
+  },
+  {
+    "revision": "349fae799f746cab8599817940f64789",
+    "url": "/portfolio-demo/static/media/1.349fae79.png"
+  },
+  {
+    "revision": "0d3c1b877c103b21dbe5ff386b517a99",
+    "url": "/portfolio-demo/static/media/2.0d3c1b87.png"
+  },
+  {
+    "revision": "7a041be7d220ad14127dd974405549b1",
+    "url": "/portfolio-demo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f8e7108f6309500a0d7b8f8ae3cf323c.js"
+  "/portfolio-demo/precache-manifest.5f7565f42a163b5d7ea79d08621c7ec5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio-demo/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
